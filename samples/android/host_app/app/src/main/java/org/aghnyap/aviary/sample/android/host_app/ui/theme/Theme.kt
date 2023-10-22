ab2fa71ea9e83c97b889687bda4d63d0bda1f3df
Aghny Arisya Putra <aghny@tuta.io>
2023-10-22 19:04:28 +0700

    style (android): set status bar color to use inverse primary

--- samples/android/host_app/app/src/main/java/org/aghnyap/aviary/sample/android/host_app/ui/theme/Theme.kt
+++ samples/android/host_app/app/src/main/java/org/aghnyap/aviary/sample/android/host_app/ui/theme/Theme.kt
@@ -82,7 +82,6 @@ private val darkColorScheme = darkColorScheme(
 @Composable
 fun HostAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
@@ -99,7 +98,7 @@ fun HostAppTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
+            window.statusBarColor = colorScheme.inversePrimary.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
